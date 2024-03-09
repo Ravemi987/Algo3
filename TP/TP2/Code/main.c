5f8988cfde849dfc332eac026cfffe196b16923f
Ravemi987 <ravemiyt@gmail.com>
2024-03-09 12:28:58 +0100

    Good version for TP2

---
+++ TP/TP2/Code/main.c
@@ -0,0 +1,39 @@
+// Need this to use the getline C function on Linux. Works without this on MacOs. Not tested on Windows.
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+
+#include "token.h"
+#include "queue.h"
+#include "stack.h"
+
+
+/** Main function for testing.
+ * The main function expects one parameter that is the file where expressions to translate are
+ * to be read.
+ *
+ * This file must contain a valid expression on each line
+ *
+ */
+int main(int argc, char **argv){
+	if (argc<2) {
+		fprintf(stderr,"usage : %s filename\n", argv[0]);
+		return 1;
+	}
+	
+	FILE *input = fopen(argv[1], "r");
+
+	if ( !input ) {
+		perror(argv[1]);
+		return 1;
+	}
+
+	computeExpressions(input);
+
+	fclose(input);
+	return 0;
+}
+ 
---
+++ TP/TP2/Code/queue.c
@@ -0,0 +1,82 @@
+/*-----------------------------------------------------------------*/
+/*
+ Licence Informatique - Structures de données
+ Mathias Paulin (Mathias.Paulin@irit.fr)
+ 
+ Implantation du TAD Queue étudié en cours.
+ 
+ */
+/*-----------------------------------------------------------------*/
+#include "queue.h"
+#include <assert.h>
+#include <stdlib.h>
+
+/* Full definition of the queue structure */
+typedef struct s_internalQueue {
+	void* value;
+	struct s_internalQueue* next;
+} InternalQueue;
+
+struct s_queue{
+	InternalQueue* head;
+	InternalQueue* tail;
+	unsigned int size;
+};
+
+Queue* createQueue(){
+	Queue* q = malloc(sizeof(Queue));
+	q->head = q->tail = NULL;
+	q->size = 0;
+	return(q);
+}
+
+void deleteQueue(ptrQueue *q) {
+	InternalQueue* toDelete = (*q)->head;
+	while (toDelete) {
+		InternalQueue* f = toDelete;
+		toDelete = toDelete->next;
+		free (f);
+	}
+	free(*q);
+	*q = NULL;
+}
+
+Queue* queuePush(Queue* q, void* v){
+	InternalQueue** insert_at = (q->size ? &(q->tail->next) : &(q->head));
+	InternalQueue* new = malloc(sizeof(InternalQueue));
+	new->value = v;
+	new->next = NULL;
+	*insert_at = new;
+	q->tail = new;
+	++(q->size);
+	return (q);
+}
+
+Queue* queuePop(Queue* q){
+	assert (!queueEmpty(q));
+	InternalQueue* old = q->head;
+	q->head = q->head->next;
+	q->size--;
+	free (old);
+	return (q);
+}
+
+void* queueTop(const Queue* q){
+	assert (!queueEmpty(q));
+	return (q->head->value);
+}
+
+bool queueEmpty(const Queue* q){
+	return (q->size == 0);
+}
+
+unsigned int queueSize(const Queue* q) {
+	return q->size;
+}
+
+void queueDump(FILE* f, const Queue* q, void(*dumpfunction)(FILE* f, const void*e)) {
+	fprintf(f, "(%d) --  ", q->size);
+	for (InternalQueue *c=q->head; c != NULL; c = c->next)
+		dumpfunction(f, c->value);
+}
+
